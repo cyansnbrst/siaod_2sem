04135851ce6bb3ab5adbf7d02864d71e90b18035
kat/ <yuuonx@mail.ru>
2022-03-31 20:18:28 +0300

    dfglfg

---
+++ task4_2/Ticket.h
@@ -0,0 +1,16 @@
+#ifndef TASK4_1_TICKET_H
+#define TASK4_1_TICKET_H
+
+#include <string>
+
+using namespace std;
+
+struct Ticket {
+
+    int train, van, seat;
+    float price;
+    string date;
+
+};
+
+#endif //TASK4_1_TICKET_H
